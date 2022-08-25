f18e3a545704786f3ea28ef3be5c3227ef49db65
Josh Goldberg <git@joshuakgoldberg.com>
2022-08-25 08:51:03 -0700

    MVP, it works yay 🤩

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Refined Saved Replies
 
 A Chrome extension for GitHub's [Saved Replies](https://docs.github.com/en/get-started/writing-on-github/working-with-saved-replies/using-saved-replies) that adds replies from a repository's `.github/replies.yml`.
+
+## TODOs
+
+- Error handling/logging
+- Investigate accessibility (how+why does GitHub use `role="none"`?)
--- src/content-script.ts
+++ src/content-script.ts
@@ -21,15 +21,64 @@ import { isBodyWithReplies } from "./validations";
   const body = await response.text();
 
   // 3. Parse the body as yml
-  const loaded = yaml.load(body);
-  if (!isBodyWithReplies(loaded)) {
-    // TODO: error case handling?
-    console.error("Invalid saved replies:", loaded);
+  const repliesConfiguration = yaml.load(body);
+  if (!isBodyWithReplies(repliesConfiguration)) {
+    console.error("Invalid saved replies:", repliesConfiguration);
     return;
   }
-  console.log({ loaded });
 
-  // 4. Add the new replies to the saved reply dropdown
+  // 4. Add a listener to modify the saved reply dropdown upon creation
+  const openSavedRepliesButton = document.getElementById(
+    "saved-reply-new_comment_field"
+  );
+  if (!openSavedRepliesButton) {
+    console.error("Couldn't find clicker");
+    return;
+  }
+  // TODO: actually hook this up to DOM event listeners? onclick?
+  openSavedRepliesButton.addEventListener("click", () => {
+    // 5. Add the new replies to the saved reply dropdown
+    // TODO: is this specific enough?
+    const replyCategoriesList = document.querySelector(
+      "markdown-toolbar fuzzy-list ul"
+    );
+    if (!replyCategoriesList) {
+      console.error("could not find fuzzy list :(");
+      return;
+    }
+
+    // TODO: Investigate using a UI framework or similar?
+    const divider = document.createElement("div");
+    divider.className = "select-menu-divider js-divider";
+    divider.textContent = "Repository replies";
+
+    // TODO: add before defaults?
+    replyCategoriesList.append(
+      divider,
+      // TODO: styling
+      // TODO: also this was messing up the list
+      ...repliesConfiguration.replies.map((reply) => {
+        const listItem = document.createElement("li");
+        listItem.setAttribute("data-value", reply.name);
+
+        const button = document.createElement("button");
+        button.textContent = "ugh";
+
+        button.addEventListener("click", (event) => {
+          event.preventDefault();
+
+          // TODO: search for this once, not on click
+          (
+            document.getElementById("new_comment_field") as HTMLTextAreaElement
+          ).value += reply.body;
+        });
+
+        listItem.appendChild(button);
+
+        return listItem;
+      })
+    );
+  });
 })().catch((error) => {
   console.error("Oh no!", error);
 });
